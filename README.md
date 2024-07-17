b0db01a4b63d4dc447cd786cf553c5ff93259397
Gideon  Addo <gideonaddoofori@gmail.com>
2024-07-17 21:30:48 +0800

    Initial project version

---
+++ README.md
@@ -0,0 +1,69 @@
+# Fullstack Notion Clone: Next.js 14, React, Convex, Tailwind
+
+![Copy of Jotter](./public/readme.png)
+
+This is a repository for Fullstack Notion Clone: Next.js 14, React, Convex, Tailwind
+
+Key Features:
+
+- Real-time database  🔗
+- Notion-style editor 📝
+- Light and Dark mode 🌓
+- Infinite children documents 🌲
+- Trash can & soft delete 🗑️
+- Authentication 🔐
+- File upload
+- File deletion
+- File replacement
+- Icons for each document (changes in real-time) 🌠
+- Expandable sidebar ➡️🔀⬅️
+- Full mobile responsiveness 📱
+- Publish your note to the web 🌐
+- Fully collapsible sidebar ↕️
+- Landing page 🛬
+- Cover image of each document 🖼️
+- Recover deleted files 🔄📄
+- Native-Like Progressive Web App for iOS 📱
+
+## Prerequisites
+
+**Node version 18.x.x**
+
+### Cloning the repository
+
+```shell
+git clone https://github.com/wuzgood98/jotter.git
+```
+
+### Install packages
+
+```shell
+npm i
+```
+
+### Setup .env file
+
+```js
+# Deployment used by `npx convex dev`
+CONVEX_DEPLOYMENT=
+NEXT_PUBLIC_CONVEX_URL=
+
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+
+EDGE_STORE_ACCESS_KEY=
+EDGE_STORE_SECRET_KEY=
+```
+
+### Setup Convex
+
+```shell
+npx convex dev
+
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,145 @@
+import "@/styles/globals.css";
+
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+import { EdgeStoreProvider } from "@/lib/edgestore";
+import { cn } from "@/lib/utils";
+
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Jotter",
+  description: "Jotter, a Notion clone.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+    apple: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon/apple-touch-icon.png",
+        href: "/favicon/apple-touch-icon.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon_dark/apple-touch-icon.png",
+        href: "/favicon_dark/apple-touch-icon.png",
+      },
+    ],
+  },
+  appleWebApp: {
+    capable: true,
+    title: "Jotter",
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      {
+        media:
+          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/iphone5_splash.png",
+      },
+      {
+        media:
+          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/iphone6_splash.png",
+      },
+      {
+        media:
+          "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/splashscreens/iphoneplus_splash.png",
+      },
+      {
+        media:
+          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/splashscreens/iphonex_splash.png",
+      },
+      {
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/iphonexr_splash.png",
+      },
+      {
+        media:
+          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+        url: "/splashscreens/iphonexsmax_splash.png",
+      },
+      {
+        media:
+          "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/ipad_splash.png",
+      },
+      {
+        media:
+          "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/ipadpro1_splash.png",
+      },
+      {
+        media:
+          "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/ipadpro3_splash.png",
+      },
+      {
+        media:
+          "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+        url: "/splashscreens/ipadpro2_splash.png",
+      },
+    ],
+  },
+  applicationName: "Jotter",
+  manifest: "https://otion-phi.vercel.app/site.webmanifest",
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#1f1f1f" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="otion-theme"
+            >
+              {children}
+              <Toaster position="bottom-right" />
+              <ModalProvider />
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
